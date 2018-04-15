0a4d1515de767c1e0e1accfaf8370e558fd25bfe
kiva2506 <kiva123@mail.ru>
2018-04-15 16:15:56 +0300

    Day1

--- Education/Education/Program.cs
+++ Education/Education/Program.cs
@@ -9,8 +9,19 @@ namespace Education
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 1;
+            p1.sym = '*';
+
+            Draw(p1.x, p1.y, p1.sym);
+
             Console.ReadKey();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
